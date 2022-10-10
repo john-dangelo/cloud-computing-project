44562ac203a2598a66e6b11c8eada3c77ecbc0c6
TrietCao <triet12369@gmail.com>
2022-10-10 14:38:46 -0500

    update service deploy

--- components/web-docker/deploy.sh
+++ components/web-docker/deploy.sh
@@ -2,4 +2,7 @@
 
 tag=$1
 
-docker build -t $tag . && docker tag $tag "managernode:5000/$tag" && docker push "managernode:5000/$tag"
+docker build -t $tag . && \
+docker tag $tag "managernode:5000/$tag" && \
+docker push "managernode:5000/$tag" && \
+docker service update --image "managernode:5000/$tag" webservice
