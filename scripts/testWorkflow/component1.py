f5b051197d8db0b15e01043b1aef51f39c9fbd11
TrietCao <triet12369@gmail.com>
2022-12-09 14:04:15 -0600

    int cast

--- scripts/testWorkflow/component1.py
+++ scripts/testWorkflow/component1.py
@@ -10,7 +10,7 @@ def container_main(data, send, args):
     # send data to the next component
     end = 10
     if (args[1]):
-        end = args[1]
+        end = int(args[1])
     for i in range(0, end):
         time.sleep(1)
         send({ "data": "Hello from component 1: " + str(i)})    
