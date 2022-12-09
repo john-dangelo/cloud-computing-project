1077075414b6c7ae2238e86e2f6db19e1df75c8e
TrietCao <triet12369@gmail.com>
2022-12-09 12:47:59 -0600

    update loop

--- scripts/testWorkflow/component1.py
+++ scripts/testWorkflow/component1.py
@@ -1,7 +1,14 @@
+import time
+
+# Mock data provider that will continuously send new data
+# Automatically ends after a set time
 def container_main(data, send):
     print("Component 1 is called with data: ", data)
     # Process data
     processed_data = data
     # send data to the next component
+    for i in range(0, 10):
+        time.sleep(1)
+        send({ "data": "Hello from component 1: " + i})    
     send("~~EOD~~")
     return "Component 1 finished processing"
